87b799a37af6f269f376a0645b9666e9a7583b3f
Wright <calebogundiya@gmail.com>
2020-07-06 00:13:46 +0100

    Added feature to get all adopters overcoming array limitations

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract Adoption {
+    // Declare public variable - get automatic getter methods
+    // but beacuse we have an array, the getter needs a key and we can only return a single value
     address[16] public adopters;
 
+    // Adopting a pet
+    // msg.sender is the address of the person or contract calling this function
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
 
@@ -10,4 +14,13 @@ contract Adoption {
 
         return petId;
     }
+
+    // Retreiving the adopters
+    // memory gives data location of the adopters variable
+    // view means function doesnt modify state of contract
+    function getadopters() public view returns (address[16] memory){
+        return adopters;
+    }
+
+    
 }
